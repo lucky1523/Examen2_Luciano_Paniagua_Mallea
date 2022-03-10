9e5243c9e5ba9a9dcc65c2b7da06f0b0e42e11e9
Luciano Paniagua <erickmallea12345@gmail.com>
2022-03-10 08:31:51 -0400

    Tercer Commit

---
+++ src/test/java/RESTAsssured/helpers/Configuration.java
@@ -0,0 +1,7 @@
+package RESTAsssured.helpers;
+
+public class Configuration {
+    public static String host="https://todo.ly";
+    public static String user="erickmallea1234@gmail.com";
+    public static String pwd="12345";
+}
